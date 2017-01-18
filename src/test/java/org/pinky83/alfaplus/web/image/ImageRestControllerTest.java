1089659ff109144cd318664d28b25f915938614b
pinky83 <pinky8388@gmail.com>
2017-01-18 22:37:12 +0200

    add REST Controller's tests

---
+++ src/test/java/org/pinky83/alfaplus/web/image/ImageRestControllerTest.java
@@ -0,0 +1,72 @@
+package org.pinky83.alfaplus.web.image;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Дмитрий on 18.01.2017.
+ *
+ */
+public class ImageRestControllerTest {
+    @Test
+    public void get() throws Exception {
+
+    }
+
+    @Test
+    public void getAll() throws Exception {
+
+    }
+
+    @Test
+    public void getAllPaged() throws Exception {
+
+    }
+
+    @Test
+    public void getAllWithLazyFields() throws Exception {
+
+    }
+
+    @Test
+    public void delete() throws Exception {
+
+    }
+
+    @Test
+    public void update() throws Exception {
+
+    }
+
+    @Test
+    public void create() throws Exception {
+
+    }
+
+    @Test
+    public void getWithLazyFields() throws Exception {
+
+    }
+
+    @Test
+    public void getLastDay() throws Exception {
+
+    }
+
+    @Test
+    public void getAllByDate() throws Exception {
+
+    }
+
+    @Test
+    public void getNextPage() throws Exception {
+
+    }
+
+    @Test
+    public void getPreviousPage() throws Exception {
+
+    }
+
+}
---
+++ src/test/java/org/pinky83/alfaplus/web/patient/PatientRestControllerTest.java
@@ -0,0 +1,57 @@
+package org.pinky83.alfaplus.web.patient;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Дмитрий on 18.01.2017.
+ *
+ */
+public class PatientRestControllerTest {
+    @Test
+    public void get() throws Exception {
+
+    }
+
+    @Test
+    public void getWithImages() throws Exception {
+
+    }
+
+    @Test
+    public void getAll() throws Exception {
+
+    }
+
+    @Test
+    public void getAllWithImages() throws Exception {
+
+    }
+
+    @Test
+    public void getAllByName() throws Exception {
+
+    }
+
+    @Test
+    public void getFilteredAll() throws Exception {
+
+    }
+
+    @Test
+    public void create() throws Exception {
+
+    }
+
+    @Test
+    public void delete() throws Exception {
+
+    }
+
+    @Test
+    public void update() throws Exception {
+
+    }
+
+}
